e936078ecbcfcd37a9dc71850da8908aecba19cf
nagomiko <nagomiko@m.local>
2018-07-20 11:15:47 +0900

    first commit

---
+++ BP.py
@@ -0,0 +1,43 @@
+import random
+
+INPUT_UNIT_NO = 2
+HIDDEN_UNIT_NO = 2
+OUTPUT_UNIT_NO = 1
+
+ETA = 0.1
+PATTERN_NO = 4
+
+TRAINING_NO = 20000
+ERROR_MAX = 0.001
+
+
+def random():
+    return random.randint(0, 1)
+
+
+def sigmoid_func(net):
+    pass
+
+
+def read_data(pattern_in, pattern_out):
+    pass
+
+
+def init_w(v, w):
+
+
+
+def forward_propagation(p, v, w, pattern_in, h_out, o_out):
+    pass
+
+
+def back_propagation(p, v, w, h_out, o_out, pattern_in, pattern_out):
+    pass
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
