9dd35a4aca60fd6dc09c6828874809badefe99b6
nagomiko <nagomiko@m.local>
2018-07-27 11:43:37 +0900

    add args

--- BP.py
+++ BP.py
@@ -1,4 +1,7 @@
+import argparse
 import random
+import math
+import sys
 
 INPUT_UNIT_NO = 2
 HIDDEN_UNIT_NO = 2
@@ -10,13 +13,15 @@ PATTERN_NO = 4
 TRAINING_NO = 20000
 ERROR_MAX = 0.001
 
+e = math.e
 
-def random():
+
+def gen_random():
     return random.randint(0, 1)
 
 
 def sigmoid_func(net):
-    pass
+    return 1 / (1 + e ** -net)
 
 
 def read_data(pattern_in, pattern_out):
@@ -26,13 +31,27 @@ def read_data(pattern_in, pattern_out):
 def init_w(v, w):
 
 
-
 def forward_propagation(p, v, w, pattern_in, h_out, o_out):
-    pass
+
 
 
 def back_propagation(p, v, w, h_out, o_out, pattern_in, pattern_out):
     pass
+
+
+def get_args():
+    # 準備
+    parser = argparse.ArgumentParser()
+
+    # 標準入力以外の場合
+    if sys.stdin.isatty():
+        parser.add_argument('output_file', help='please set ./a.out', type=str)
+        parser.add_argument('input_file', help='please set xor.dat', type=str)
+
+    # 結果を受ける
+    args = parser.parse_args()
+
+    return (args)
 
 
 def main():
